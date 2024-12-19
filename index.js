808504eb911f076572dce0e030fa9e5970438ae0
Satendra Rajput <satendracaria@gmail.com>
2024-12-19 17:06:34 +0530

    IP_1

--- index.js
+++ index.js
@@ -3,16 +3,17 @@ const os = require('os');
 const app = express();
 
 app.get('/api/os-info', (req, res) => {
+    const userIP = req.ip || req.connection.remoteAddress; // User's IP address
     const osInfo = {
         platform: os.platform(),
         release: os.release(),
         arch: os.arch(),
         hostname: os.hostname(),
         totalMemory: os.totalmem(),
-        freeMemory: os.freemem()
+        freeMemory: os.freemem(),
+        userIP: userIP // Add user IP to the response
     };
     res.json(osInfo);
 });
-
 
 app.listen(3000, () => console.log('Server running on port 3000'));
